f94cbb12a8ebae3526184544671877d867aad527
Pushpa Kumari <112460597+Pushpa-creator@users.noreply.github.com>
2022-11-03 18:17:31 +0530

    Now prime code is working

--- prime_problem.js
+++ prime_problem.js
@@ -1,16 +1,18 @@
 function checkPrime(num){
-let count=0;
-     for(let i=1; i<=num; i++){
-         if (num%i==0){
-             count++;
-           }
+  let count=0;
+  for(let i=1; i<=num; i++){
+      if (num%i==0){
+       count++;
+      }
+  }
 if(count==2){
-return true;
-       }
-else {return false;}
-}
+   return true;
+}else {
+   return false;}
 }
 let ans =checkPrime(14)
-if(ans==true){console.log("prime");}
-else {console.log("not prime")
+if(ans==true){
+console.log("Prime");
+}else {
+console.log("Not Prime")
 }
